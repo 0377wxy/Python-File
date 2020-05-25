3d673ed48104d7ea2b13d78ba789f0c890d6cd59
wxy <1165205374@qq.com>
2020-05-25 15:56:01 +0800

    删除

--- 数据结构与算法-文件/402.py
+++
@@ -1,9 +0,0 @@
-def toStr(n, base):
-    converString = "0123456789ABCDEF"
-    if n < base:
-        return converString[n]
-    else:
-        return toStr(n // base, base) + converString[n % base]
-
-
-print(toStr(1453, 16))
