84d193a28e811035c7b634154e6b76757b243772
0377wxy <1165205374@qq.com>
2023-09-09 23:58:58 +0800

    ..

---
+++ temp/1.7.4.py
@@ -0,0 +1,17 @@
+ls = [2, 3, 4, ]
+d = ls[1]-4
+print(ls, d)
+print(12, end=" "*3)
+a = 1234
+
+b = str(a)
+
+print(len(b))
+
+a = "abcdabcabcac"
+b = "abca"
+ls = [0, 1, 2, 3, 4, 5, 6]
+ls2 = ls[0:3]+ls[4:]
+ls3 = a[:2]+a[5:]
+
+print(ls2, ls3)
